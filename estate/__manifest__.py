837918fbf4583eda9fa94e2e32c724973159703f
abdulrahman <abdulrahmanfo25@gmail.com>
2025-08-17 15:32:57 +0300

    Adjust manifest file to make the app appear

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+
+{
+    'name': 'Real Estate',
+    'author': 'Abdulrahman Fouad',
+    'version': '18.0.0.1.0',
+    'category': 'Training',
+    'summary': 'My first module for training on odoo 18',
+    'sequence': 10,
+    'description': """Real Estate module training module from Odoo docs""",
+    'depends': ['base'],
+    'application': True,
+}
