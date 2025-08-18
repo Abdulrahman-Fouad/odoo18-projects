e9163ed2c38842fd090085c53ca7b56df888edd3
abdulrahman <abdulrahmanfo25@gmail.com>
2025-08-18 09:39:14 +0300

    Security Brief Introduction

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,10 +3,14 @@
     'name': 'Real Estate',
     'author': 'Abdulrahman Fouad',
     'version': '18.0.0.1.0',
+    'license': 'LGPL-3',
     'category': 'Training',
     'summary': 'My first module for training on odoo 18',
     'sequence': 10,
     'description': """Real Estate module training module from Odoo docs""",
     'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv'
+    ],
     'application': True,
 }
