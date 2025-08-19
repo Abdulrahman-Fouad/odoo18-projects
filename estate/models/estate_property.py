3c8189190b8f14c697d8a15e52f235d76428473f
abdulrahman <abdulrahmanfo25@gmail.com>
2025-08-19 13:45:02 +0300

    Relations Between Models (Many2one, Many2many & One2many)

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -34,3 +34,11 @@ class EstateProperty(models.Model):
         ('sold','Sold'),
         ('cancelled','Cancelled'),
     ], required=True, copy=False, default='new')
+
+    property_type_id = fields.Many2one("estate.property.type", string="Type")
+    salesperson_id = fields.Many2one('res.users', string="Salesman", default=lambda self: self.env.user)
+    buyer_id = fields.Many2one('res.partner', string="Buyer", copy=False)
+
+    tag_ids = fields.Many2many('estate.property.tag')
+
+    offer_ids = fields.One2many('estate.property.offer', 'property_id')
