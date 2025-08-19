9dd5fb3b5437c035c047cf5e7201b242e7ade4c1
abdulrahman <abdulrahmanfo25@gmail.com>
2025-08-19 18:56:47 +0300

    Chapter 8: Computed Fields And Onchanges

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,8 +1,7 @@
-from email.policy import default
-
 from dateutil import relativedelta
 from dateutil.utils import today
-from odoo import fields, models
+from odoo import fields, models, api
+
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
@@ -11,7 +10,7 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default= today() + relativedelta.relativedelta(months=3))
+    date_availability = fields.Date(copy=False, default=today() + relativedelta.relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
@@ -25,14 +24,14 @@ class EstateProperty(models.Model):
         ('south', 'South'),
         ('east', 'East'),
         ('west', 'West'),
-        ])
+    ])
     active = fields.Boolean(default=True)
     state = fields.Selection([
-        ('new','New'),
-        ('offer_received','Offer Received'),
-        ('offer_accepted','Offer Accepted'),
-        ('sold','Sold'),
-        ('cancelled','Cancelled'),
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('cancelled', 'Cancelled'),
     ], required=True, copy=False, default='new')
 
     property_type_id = fields.Many2one("estate.property.type", string="Type")
@@ -42,3 +41,24 @@ class EstateProperty(models.Model):
     tag_ids = fields.Many2many('estate.property.tag')
 
     offer_ids = fields.One2many('estate.property.offer', 'property_id')
+
+    total_area = fields.Integer(compute="_compute_total_area")
+    best_price = fields.Float(compute="_compute_best_offer")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends("offer_ids")
+    def _compute_best_offer(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = max(record.offer_ids.mapped('price'))
+            else:
+                record.best_price = 0
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        self.garden_area = 10 if self.garden else False
+        self.garden_orientation = 'north' if self.garden else False
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,13 +1,32 @@
-from odoo import fields, models
+from dateutil import relativedelta
+
+from odoo import fields, models, api
+from odoo.fields import Datetime
+
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Estate Property Offer"
 
-    name = fields.Char(required=True)
+    name = fields.Char()
     price = fields.Float()
     status = fields.Selection([
         ('accepted','Accepted'),
         ('refused','Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
+
+    validity = fields.Integer(default=7, string="Validity (days)")
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline", string="Deadline")
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            create_date = record.create_date.date() if record.create_date else fields.Date().today()
+            record.date_deadline = create_date + relativedelta.relativedelta(days= record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.date_deadline:
+                create_date = record.create_date.date() if record.create_date else fields.Date().today()
+                record.validity = (record.date_deadline - record.create_date.date()).days
