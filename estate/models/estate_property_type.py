0c361c201982352c8d9c1cc5471ae5f5bd1ca60f
abdulrahman <abdulrahmanfo25@gmail.com>
2025-08-20 16:47:15 +0300

    Chapter 11: Add Inline Views

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,7 +6,7 @@ class EstatePropertyType(models.Model):
     _description = 'Estate Property type'
 
     name = fields.Char(required=True)
-
+    property_ids = fields.One2many('estate.property', 'property_type_id')
 
     _sql_constraints = [
         ('unique_type_name', 'UNIQUE(name)', 'This Type already exists!!')
