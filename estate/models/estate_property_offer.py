707805871d634913956314e3a028b1a301c64d33
abdulrahman <abdulrahmanfo25@gmail.com>
2025-08-28 11:12:49 +0300

    Chapter 12: Model & View Inheritance (Add property_ids field to Users [add domain to only lists the available properties], Add the property_ids field to the base.view_users_form in a new notebook page)

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -15,7 +15,7 @@ class EstatePropertyOffer(models.Model):
         ('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
-    property_type_id = fields.Many2one(related='property_id.property_type_id', stored=True)
+    property_type_id = fields.Many2one(related='property_id.property_type_id', store=True)
 
     validity = fields.Integer(default=7, string="Validity (days)")
     date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline", string="Deadline")
